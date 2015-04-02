8b241e30999020d117e7bcc7e083172b873341f8
Tim Kuijsten <tim@netsend.nl>
2015-04-03 00:48:34 +0200

    refs #79: csp helpers.swig

---
+++ source/vendors/csp/helpers.js
@@ -0,0 +1,23 @@
+function hasMobileUA () {
+  var nav = window.navigator;
+  var ua = nav.userAgent;
+  var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
+
+  return pa.test(ua);
+}
+
+function isDesktop () {
+  return screen.width > 991 && !hasMobileUA();
+}
+
+function isTablet () {
+  return screen.width < 992 && screen.width > 767 && hasMobileUA();
+}
+
+function isMobile () {
+  return screen.width < 767 && hasMobileUA();
+}
+
+function escapeSelector (selector) {
+  return selector.replace(/[!"$%&'()*+,.\/:;<=>?@[\\\]^`{|}~]/g, "\\$&")
+}
