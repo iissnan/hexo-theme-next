994ca45cf7d610be759ff28eed0d4b9e7200cdd8
iissnan <iissnan@gmail.com>
2015-06-03 10:16:52 +0800

    Bug: Baidu analytics is not installed correctly. #52

--- source/js/analytics_baidu-analytics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?" + CONFIG.analytics.baidu;
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
