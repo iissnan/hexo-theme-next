e912ab95c4eb96490b7f2314384180d938d1b0af
kpavlov <git@konstantinpavlov.net>
2015-07-11 07:39:20 +0300

    inline google analytics code

--- source/js/analytics_google-analytics.js
+++
@@ -1,6 +0,0 @@
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-  e.src='//www.google-analytics.com/analytics.js';
-  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-ga('create',CONFIG.analytics.google);ga('send','pageview');
