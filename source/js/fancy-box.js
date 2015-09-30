7aa1095de2ab9890c1a6e8dfb9ab901119cc84ac
iissnan <iissnan@gmail.com>
2015-09-30 15:07:32 +0800

    Feature: Group Picture for Archive and Details page. #295

--- source/js/fancy-box.js
+++ source/js/fancy-box.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-  $('.content img').each(function () {
+  $('.content img').not('.group-picture img').each(function () {
     var $image = $(this);
     var $imageWrapLink = $image.parent('a');
 
     if ($imageWrapLink.size() < 1) {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
+
     $imageWrapLink.addClass('fancybox');
-    if(this.title){
-      $imageWrapLink.attr("title",this.title); //make sure img title tag will show correctly in fancybox
+
+    //make sure img title tag will show correctly in fancybox
+    if (this.title) {
+      $imageWrapLink.attr("title", this.title);
     }
+
   });
 });
 $('.fancybox').fancybox({
