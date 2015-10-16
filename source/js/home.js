11fc255e27ba51868fa45946fa2e70852d3fe9d0
Andy Shu Xin <andyshuxin@gmail.com>
2015-10-16 14:57:56 +0800

    add event parament to google analytics tracking code

--- source/js/home.js
+++ source/js/home.js
@@ -58,7 +58,7 @@
     window.history.pushState("", document.title, window.location.pathname + window.location.search);
     console.log(lightbox)
 
-    ga('send', 'lightbox', 'close', lightbox.id);
+    ga('send', 'event', 'lightbox', 'close', lightbox.id);
   }
 
   function configLightbox(project){
@@ -76,7 +76,7 @@
       window.location.hash = event.target.parentNode.id.replace('anchor-', '');
       window.currentLightbox = lightbox;
 
-      ga('send', 'lightbox', 'open', lightbox.id);
+      ga('send', 'event', 'lightbox', 'open', lightbox.id);
 
     });
 
