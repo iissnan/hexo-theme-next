09f9f11de5ec8fa5cfb3313591225fdae3a19c06
iissnan <iissnan@gmail.com>
2015-11-15 23:46:47 +0800

    Script: Refactor duoshuo hook.

--- source/js/src/hook-duoshuo.js
+++ source/js/src/hook-duoshuo.js
@@ -1,8 +1,7 @@
-if (typeof DUOSHUO !== 'undefined') {
-  hookDUOSHUO_tp();
-} else {
-  $('#duoshuo-script')[0].onload = hookDUOSHUO_tp;
-}
+typeof DUOSHUO !== 'undefined' ?
+  hookDUOSHUO_tp() :
+  ($('#duoshuo-script')[0].onload = hookDUOSHUO_tp);
+
 
 function hookDUOSHUO_tp() {
   var _D_post = DUOSHUO.templates.post;
@@ -10,61 +9,88 @@ function hookDUOSHUO_tp() {
   DUOSHUO.templates.post = function (e, t) {
     var rs = _D_post(e, t);
     var agent = e.post.agent;
-    var isAdmin;
-    if (typeof duoshuo_user_ID !== 'undefined') {
-      if (e.post.author.user_id && (e.post.author.user_id == duoshuo_user_ID)) {
-        if (duoshuo_admin_nickname) {
-          isAdmin = '<span class="fa">' + duoshuo_admin_nickname + '</span>';
-        } else {
-          isAdmin = '<span class="fa">博主</span>';
-        }
-      } else {
-        isAdmin = '';
-      }
-    } else {
-      isAdmin = '';
+    var userId = e.post.author.user_id;
+    var admin = '';
+
+    if (userId && (userId == duoshuo_user_ID)) {
+      admin = '<span class="duoshuo-ua-admin">' + duoshuo_admin_nickname + '</span>';
     }
-    if (agent && /^Mozilla/.test(agent)) rs = rs.replace(/<\/div><p>/, isAdmin + showUa(agent) + '</div><p>');
+
+    if (agent && /^Mozilla/.test(agent)) {
+      rs = rs.replace(/<\/div><p>/, admin + getAgentInfo(agent) + '</div><p>');
+    }
+
     return rs;
   }
 }
 
-//移动客户端判断
-function checkMobile() {
-  var isiPad = navigator.userAgent.match(/iPad/i) !== null;
-  if (isiPad) {
-    return false;
-  }
-  var isMobile = navigator.userAgent.match(/iphone|android|phone|mobile|wap|netfront|x11|java|opera mobi|opera mini|ucweb|windows ce|symbian|symbianos|series|webos|sony|blackberry|dopod|nokia|samsung|palmsource|xda|pieplus|meizu|midp|cldc|motorola|foma|docomo|up.browser|up.link|blazer|helio|hosin|huawei|novarra|coolpad|webos|techfaith|palmsource|alcatel|amoi|ktouch|nexian|ericsson|philips|sagem|wellcom|bunjalloo|maui|smartphone|iemobile|spice|bird|zte-|longcos|pantech|gionee|portalmmm|jig browser|hiptop|benq|haier|^lct|320x320|240x320|176x220/i) != null;
-  if (isMobile) {
-    return true;
-  }
-  return false;
-}
-
-function showUa(string) {
+function getAgentInfo(string) {
   $.ua.set(string);
+
+  var UNKNOWN = 'Unknown';
   var sua = $.ua;
-  var br = '&nbsp;&nbsp;';
-  var osIco = '<i class="fa fa-desktop"></i>&nbsp;';
-  var browserIco = '<i class="fa fa-globe"></i>&nbsp;'
-  var osName = sua.os.name;
-  var browserName = sua.browser.name;
-  if (checkMobile()) br = '<br><br>';
+  var seperator = isMobile() ? '<br><br>' : '<span class="duoshuo-ua-seperator"></span>';
+  var osName = sua.os.name || UNKNOWN;
+  var osVersion = sua.os.version  || UNKNOWN;
+  var browserName = sua.browser.name || UNKNOWN;
+  var browserVersion = sua.browser.version || UNKNOWN;
+  var iconMapping = {
+    os: {
+      android : 'android',
+      linux   : 'linux',
+      windows : 'windows',
+      ios     : 'apple',
+      'mac os': 'apple',
+      unknown : 'desktop'
+    },
+    browser: {
+      chrome : 'chrome',
+      'chromium': 'chrome',
+      firefox: 'firefox',
+      opera  : 'opera',
+      safari : 'safari',
+      ie     : 'internet-explorer',
+      unknown: 'globe'
+    }
+  };
+  var osIcon = iconMapping.os[osName.toLowerCase()];
+  var browserIcon =  iconMapping.browser[browserName.toLowerCase()];
 
-  if (osName == 'Android') osIco = '<i class="fa fa-android"></i>&nbsp;';
-  if (osName.match(/linux/i)) osIco = '<i class="fa fa-linux"></i>&nbsp;';
-  if (osName.match(/mac os|ios/i)) osIco = '<i class="fa fa-apple"></i>&nbsp;';
-  if (sua.os.version == 'x86_64') sua.os.version = 'x64';
+  return seperator +
+    '<span class="duoshuo-ua-platform duoshuo-ua-platform-' + osName.toLowerCase() + '">' +
+       '<i class="fa fa-' + osIcon + '"></i>' +
+       osName + ' ' + osVersion +
+    '</span>' + seperator +
+    '<span class="duoshuo-ua-browser duoshuo-ua-browser-' + browserName.toLowerCase() + '">' +
+      '<i class="fa fa-' + browserIcon + '"></i>' +
+      browserName + ' ' + browserVersion +
+    '</span>';
 
-  if (browserName.match(/chrome|chromium/i)) browserIco = '<i class="fa fa-chrome"></i>&nbsp;';
-  if (browserName.match(/firefox/i)) browserIco = '<i class="fa fa-firefox"></i>&nbsp;';
-  if (browserName.match(/opera/i)) browserIco = '<i class="fa fa-opera"></i>&nbsp;';
-  if (browserName.match(/safari/i)) browserIco = '<i class="fa fa-safari"></i>&nbsp;';
-  if (browserName.match(/ie/i)) browserIco = '<i class="fa fa-internet-explorer"></i>&nbsp;';
+  function isMobile() {
+    var userAgent = navigator.userAgent;
 
-  return br + '<span class="platform ' + sua.os.name + '">' + osIco +
-    sua.os.name + ' ' + sua.os.version + '</span>' + br +
-    '<span class="browser ' + sua.browser.name + '">' + browserIco +
-    sua.browser.name + '|' + sua.browser.version + '</span>';
+    var isiPad = userAgent.match(/iPad/i) !== null;
+    var mobileUA = [
+      'iphone', 'android', 'phone', 'mobile',
+      'wap', 'netfront', 'x11', 'java', 'opera mobi',
+      'opera mini', 'ucweb', 'windows ce', 'symbian',
+      'symbianos', 'series', 'webos', 'sony',
+      'blackberry', 'dopod', 'nokia', 'samsung',
+      'palmsource', 'xda', 'pieplus', 'meizu',
+      'midp' ,'cldc' , 'motorola', 'foma',
+      'docomo', 'up.browser', 'up.link', 'blazer',
+      'helio', 'hosin', 'huawei', 'novarra',
+      'coolpad', 'webos', 'techfaith', 'palmsource',
+      'alcatel', 'amoi', 'ktouch', 'nexian',
+      'ericsson', 'philips', 'sagem', 'wellcom',
+      'bunjalloo', 'maui', 'smartphone', 'iemobile',
+      'spice', 'bird', 'zte-', 'longcos',
+      'pantech', 'gionee', 'portalmmm', 'jig browser',
+      'hiptop', 'benq', 'haier', '^lct',
+      '320x320', '240x320', '176x220'
+    ];
+    var pattern = new RegExp(mobileUA.join('|'), 'i');
+
+    return !isiPad && userAgent.match(pattern);
+  }
 }
