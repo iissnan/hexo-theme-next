b703e6a9dd68c7485bc0d39d2f512babd1e23074
idhyt <idhyt@hotmail.com>
2015-12-30 00:18:36 +0800

    my next

--- source/js/fancy-box.js
+++ source/js/fancy-box.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-  $('.content img').each(function () {
+  $('.content img').not('.group-picture img').each(function () {
+
     var $image = $(this);
+    var imageTitle = $image.attr('title');
     var $imageWrapLink = $image.parent('a');
 
     if ($imageWrapLink.size() < 1) {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
+
     $imageWrapLink.addClass('fancybox');
-    if(this.title){
-      $imageWrapLink.attr("title",this.title); //make sure img title tag will show correctly in fancybox
+    $imageWrapLink.attr('rel', 'group');
+
+    if (imageTitle) {
+      $imageWrapLink.append('<p class="image-caption">' + imageTitle + '</p>');
+      $imageWrapLink.attr("title", imageTitle); //make sure img title tag will show correctly in fancybox
     }
   });
 });
+
 $('.fancybox').fancybox({
   helpers: {
     overlay: {
