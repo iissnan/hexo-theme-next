3eaa72cdec0554964c3092990e411e4b010e0f76
kisnows <yq12315@gmail.com>
2016-01-14 20:21:35 +0800

    页面滚动时固定sidebar

--- source/js/src/schemes/pisces.js
+++ source/js/src/schemes/pisces.js
@@ -5,13 +5,22 @@ $(document).ready(function () {
   var sidebarTop = headerHeight + 10;
 
   $sidebar
-    .css({ 'margin-top': sidebarTop })
+    .css({'margin-top': sidebarTop})
     .show();
 
   NexT.utils.isDesktop() && $('.page-post-detail .sidebar-inner').affix({
     offset: {
       top: sidebarTop,
       bottom: footerOffset
+    }
+  });
+
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > headerHeight) {
+      $('#sidebar').addClass('fixed-sidebar');
+    } else {
+      $('#sidebar').removeClass('fixed-sidebar');
     }
   });
 });
