571b9aeb17162916399dd963f38a9ee21485bd84
Jacks <191100479@qq.com>
2016-09-02 15:38:56 +0800

    Fix the css and Compatible with mobile terminal

--- source/extensions/study/study.js
+++ source/extensions/study/study.js
@@ -2,9 +2,27 @@
  * Created by Jacks on 2016/8/20.
  */
 $(function () {
+  //region 横竖屏判断
+  var card = $('.card');
+
+  function hengshuping() {
+    if (window.orientation == 180 || window.orientation == 0) {
+      // alert("竖屏状态！")
+    }
+    if (window.orientation == 90 || window.orientation == -90) {
+      // alert("横屏状态！")
+    }
+    var visible_width = $('.tab-content').children(':visible').children().width();
+    card.height(visible_width * 1.414);
+
+  }
+
+  window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
+  //endregion
+
   $(window).load(function () {
-    var card = $('.card');
-    card.height(card.width() * 1.414);
+    var visible_width = $('.tab-content').children(':visible').children().width();
+    card.height(visible_width * 1.414);
   });
 
   $('.tab-block').on('click', function (e) {
@@ -44,6 +62,7 @@ $(function () {
       top: y + 'px',
       left: x + 'px'
     }).addClass("rippleEffect");
+
 
   });
 });
