9a71ac950cc840e65ed416f5cf1a72d3e91e337d
Ivan.Nginx <ivan.nginx@gmail.com>
2017-02-20 00:39:41 +0300

    ADD: scroll percent in b2t [js].

--- source/js/src/utils.js
+++ source/js/src/utils.js
@@ -46,6 +46,13 @@ NexT.utils = NexT.$u = {
 
     $(window).on('scroll', function () {
       $top.toggleClass('back-to-top-on', window.pageYOffset > THRESHOLD);
+      
+      var scrollTop = $(window).scrollTop();
+      var docHeight = $(document).height();
+      var winHeight = $(window).height();
+      var scrollPercent = (scrollTop) / (docHeight - winHeight);
+      var scrollPercentRounded = Math.round(scrollPercent*100);
+      $('#scrollpercent>span').html(scrollPercentRounded);
     });
 
     $top.on('click', function () {
