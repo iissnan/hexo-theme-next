dc1f7c186a22dcb65dc3d05db8a6f0a244eb8b5c
iissnan <iissnan@gmail.com>
2017-03-14 03:06:43 +0800

    Bug: can't not found module hexo-util. #1500

--- scripts/tags/exturl.js
+++ scripts/tags/exturl.js
@@ -5,10 +5,9 @@
 'use strict';
 
 /*jshint camelcase: false */
-var util = require(hexo.base_dir + 'node_modules/hexo/node_modules/hexo-util');
+var util = require(hexo.base_dir + 'node_modules/hexo-util');
 /*jshint camelcase: true */
 var htmlTag = util.htmlTag;
-//console.log(util);
 
 var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
 
