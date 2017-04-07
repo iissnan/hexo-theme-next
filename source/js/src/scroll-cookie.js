35b9648da270cbd5460d42fa7b3c34faf921bc87
Ivan.Nginx <ivan.nginx@gmail.com>
2017-04-07 14:30:37 +0300

    FIX: `save_scroll` main script file. [2]

--- source/js/src/scroll-cookie.js
+++ source/js/src/scroll-cookie.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-  // Set relative link path (without domain): https://stackoverflow.com/questions/3955959/whats-an-easy-way-to-get-the-url-in-the-current-window-minus-the-domain-name
+  // Set relative link path (without domain)
+  // https://stackoverflow.com/questions/3955959/whats-an-easy-way-to-get-the-url-in-the-current-window-minus-the-domain-name
   var rpath = window.location.href.replace(window.location.origin, "");
 
   // Write position in cookie
@@ -8,13 +9,13 @@ $(document).ready(function() {
   $(window).on("scroll", function() {
     clearTimeout(timeout);
     timeout = setTimeout(function () {
-      Cookies.set('scroll-cookie', ($(window).scrollTop() + '|' + rpath), { path: '' });
+      Cookies.set("scroll-cookie", ($(window).scrollTop() + '|' + rpath), { path: '' });
     }, 250);
   });
 
   // Read position from cookie
   if (Cookies.get("scroll-cookie") !== undefined) {
-    var cvalues = Cookies.get('scroll-cookie').split('|');
+    var cvalues = Cookies.get("scroll-cookie").split('|');
       if (cvalues[1] == rpath) {
         $(window).scrollTop(cvalues[0]);
       }
