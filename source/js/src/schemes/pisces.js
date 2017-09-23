920bd39f3e3b2439e514c6cef93ea83a43433a06
Ivan.Nginx <ivan.nginx@gmail.com>
2017-09-23 15:30:57 +0300

    ADD: sidebar scrollable in all schemes [14].
    Add `resizeListener()` to sidebar.

--- source/js/src/schemes/pisces.js
+++ source/js/src/schemes/pisces.js
@@ -1,20 +1,52 @@
 $(document).ready(function () {
 
+  var sidebarInner = $('.sidebar-inner');
+
   initAffix();
+  resizeListener();
 
   function initAffix () {
-    var headerOffset = $('.header-inner').height() + CONFIG.sidebar.offset,
-        footerInner = $('.footer-inner'),
-        footerMargin = footerInner.outerHeight(true) - footerInner.outerHeight(),
-        footerOffset = footerInner.outerHeight(true) + footerMargin;
+    var headerOffset = getHeaderOffset(),
+        footerOffset = getFooterOffset();
 
-    $('.sidebar-inner').affix({
+    sidebarInner.affix({
       offset: {
         top: headerOffset - CONFIG.sidebar.offset,
         bottom: footerOffset
       }
     });
 
-    $('#sidebar').css({ 'margin-top': headerOffset + 'px' }).show();
+    setSidebarMarginTop(headerOffset).show();
   }
+
+  function resizeListener () {
+    var mql = window.matchMedia('(min-width: 991px)')
+    mql.addListener(function(e){
+      if(e.matches){
+        recalculateAffixPosition();
+      }
+    });
+  }
+
+  function getHeaderOffset () {
+    return $('.header-inner').height() + CONFIG.sidebar.offset;
+  }
+
+  function getFooterOffset () {
+    var footerInner = $('.footer-inner'),
+        footerMargin = footerInner.outerHeight(true) - footerInner.outerHeight(),
+        footerOffset = footerInner.outerHeight(true) + footerMargin;
+    return footerOffset;
+  }
+
+  function setSidebarMarginTop (headerOffset) {
+    return $('#sidebar').css({ 'margin-top': headerOffset });
+  }
+
+  function recalculateAffixPosition () {
+    $(window).off('.affix')
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom')
+    initAffix();
+  }
+
 });
