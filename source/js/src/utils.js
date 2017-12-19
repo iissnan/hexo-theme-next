43d0ba32b4d3b9546b151b3b70a6e067d51bdc4b
Rainy <geekrainy@gmail.com>
2017-12-19 15:14:12 +0800

    Fix issue 2042.

--- source/js/src/utils.js
+++ source/js/src/utils.js
@@ -320,7 +320,7 @@ $(document).ready(function () {
 
     // Initialize Sidebar & TOC Width.
     var scrollbarWidth = NexT.utils.getScrollbarWidth();
-      if ($('.sidebar-panel').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
+      if ($('.sidebar-panel').height() < (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
         $('.site-overview').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
       }
     $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
